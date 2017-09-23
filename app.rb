7866f2582b2e0928385dd3d781604dedf87cb866
Jacky Alciné <yo@jacky.wtf>
2017-09-23 15:58:15 -0700

    feat(app): Autoload gems from Bundler.

--- app.rb
+++ app.rb
@@ -4,4 +4,14 @@ require 'sinatra'
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  # TODO: Create fixed list of helpers to load.
+  # TODO: Define sessions.
+  # TODO: Secure sessions by this
+  #= begin
+  # require 'sysrandom/securerandom'
+  # set :session_secret, ENV.fetch('SESSION_SECRET') { SecureRandom.hex(64) }
+  #= end
+  configure :production, :development do
+    enable :logging
+  end
 end
