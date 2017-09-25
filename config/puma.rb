3c21ee0c6c6c2d8548b5927be7a1dbf32bae5a12
Jacky Alciné <yo@jacky.wtf>
2017-09-24 22:49:50 -0700

    feat(puma): Go easy on the cores.

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,6 @@ environment ENV['RACK_ENV'].to_s
 daemonize false
 pidfile '/var/run/puma.pid'
 state_path '/tmp/puma.state'
-threads 1, 32
+threads 1, 8
 bind 'tcp://0.0.0.0:' + ENV['PORT']
 tag 'g4poc-api'
