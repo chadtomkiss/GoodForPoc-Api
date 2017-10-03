f00feeb67d0c94285bbd5044e31c7898955b36d1
Jacky Alciné <yo@jacky.wtf>
2017-10-03 09:40:43 -0700

    fix(mock-api): Ensure API can reach it.

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@
 ENV['RACK_ENV'] ||= 'production'
 
 require 'bundler'
+require 'json'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
 
 class App < Sinatra::Base
@@ -22,6 +23,14 @@ class App < Sinatra::Base
   end
 
   get '/gql' do
-    nil
+    mock_graphql_host = ENV.fetch('MOCK_GRAPHQL_SERVER_HOST',
+                                  'http://localhost:3000')
+    query = params.fetch('q', {})
+
+    begin
+      HTTParty.get("#{mock_graphql_host}/graphql/?query=#{query}").body
+    rescue => e
+      [500, JSON.dump(error: e)]
+    end
   end
 end
