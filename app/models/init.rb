7170f582099bf1fe9dd959ed1d3cdc71c169ff61
Chad <hello@chadtomkiss.co.uk>
2017-10-12 19:36:54 +0100

    feat(models): Add init.rb for models

---
+++ app/models/init.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+require_relative 'companies/benefit'
+require_relative 'companies/company'
+require_relative 'companies/location'
+require_relative 'companies/sector'
+require_relative 'questions/question'
+require_relative 'questions/question_option'
+require_relative 'questions/question_page'
+require_relative 'submissions/submission'
+require_relative 'submissions/submission_answer'
