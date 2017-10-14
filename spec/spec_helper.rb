4437df0f49b33a0b00d0ecf65d15196c09b90096
Jacky Alciné <yo@jacky.wtf>
2017-10-13 17:32:10 -0700

    fix(test): Don't run some tests in CI.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,7 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.filter_run_when_matching :focus
+  config.filter_run_excluding :no_ci if ENV['CI'] != nil
   config.disable_monkey_patching!
   config.warnings = true
   config.default_formatter = 'doc' if config.files_to_run.one?
