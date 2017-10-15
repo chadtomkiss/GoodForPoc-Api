c3b002eb3b45037afaddb80a000ed87f893592ca
Chad <hello@chadtomkiss.co.uk>
2017-10-15 16:59:00 +0100

    feat(rack-attack): Load Active Support

--- config/environment.rb
+++ config/environment.rb
@@ -9,6 +9,9 @@
 require 'bundler'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
 
+# Load Active Support
+require 'active_support/all'
+
 # Connect to DB
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
